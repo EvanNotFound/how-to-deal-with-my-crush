74a033c304654ca10c95fb0e4e1ee062e9fe3a8b
Evan Luo <contact@ohevan.com>
2024-06-29 00:53:08 -0400

    feat: add SEO title template to theme configuration

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,6 +10,11 @@ const config: DocsThemeConfig = {
   footer: {
     text: '2024 © EvanNotFound',
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – 一个自以为是的情感指南'
+    }
+  }
 }
 
 export default config
