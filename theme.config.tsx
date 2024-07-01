44bd879986f8f49c5c32fbfce2f7fd15db40735b
Evan Luo <contact@ohevan.com>
2024-06-30 20:25:12 -0400

    feat: update theme config with logo image and footer enhancements

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,9 +3,21 @@ import { useRouter } from "next/router";
 import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import Script from "next/script";
 import Link from "next/link";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span className={"text-lg font-bold"}>一个自以为是的情感指南</span>,
+  logo: (
+    <div className={"flex flex-row items-center justify-center gap-1.5"}>
+      <Image
+        src={"/icon.png"}
+        alt={"Logo"}
+        width={512}
+        height={512}
+        className={"h-6 w-6 -translate-y-[1px]"}
+      />
+      <span className={"text-lg font-bold"}>一个自以为是的情感指南</span>
+    </div>
+  ),
   project: {
     link: "https://github.com/EvanNotFound/how-to-deal-with-my-crush",
   },
@@ -13,12 +25,17 @@ const config: DocsThemeConfig = {
     "https://github.com/EvanNotFound/how-to-deal-with-my-crush",
   footer: {
     content: (
-      <p className="_text-sm">
-        <Link href={"https://creativecommons.org/licenses/by-sa/4.0/"}>
-          CC BY-SA 4.0{" "}
-        </Link>
-        © {new Date().getFullYear()} EvanNotFound
-      </p>
+      <div className={"flex w-full flex-row items-center justify-between"}>
+        <p className="_text-sm">
+          <Link href={"https://creativecommons.org/licenses/by-sa/4.0/"}>
+            CC BY-SA 4.0{" "}
+          </Link>
+          © {new Date().getFullYear()} EvanNotFound
+        </p>
+        <p className="_text-sm">
+          made with ❤️ by <Link href="https://evannotfound.com">Evan</Link>
+        </p>
+      </div>
     ),
   },
   head: function useHead() {
