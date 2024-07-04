79d6197cedbb5d92b39dc18c42a6f081051939fc
Evan Luo <contact@ohevan.com>
2024-07-03 22:53:26 -0400

    feat: add PWA support with web manifest and browser config
    - Added `site.webmanifest` for PWA
    - Added `browserconfig.xml` for Windows tiles
    - Updated `theme.config.tsx` to include icons and manifest links
    - Added `safari-pinned-tab.svg` for Safari pinned tabs

--- theme.config.tsx
+++ theme.config.tsx
@@ -51,11 +51,32 @@ const config: DocsThemeConfig = {
       <>
         <title>{title}</title>
         <link rel="shortcut icon" href="/icon.png" type="image/x-icon" />
-        <link rel="icon" href="/icon.png" type="image/x-icon" />
         <meta property="og:title" content={title} />
         <meta name="description" content={description} />
         <meta property="og:description" content={description} />
         <meta property={"og:image"} content={"/opengraph.png"} />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </>
     );
   },
