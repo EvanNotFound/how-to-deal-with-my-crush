d8af69108e599b2df9b9bcb065cb3828d12b751c
Evan Luo <contact@ohevan.com>
2024-12-24 01:09:07 -0500

    feat: update desc

--- pages/tutorial-before/_meta.ts
+++ pages/tutorial-before/_meta.ts
@@ -2,4 +2,5 @@ export default {
   "how-to-not-make-a-clown-of-yourself": "如何不当小丑",
   "how-to-talk-to-girls": "如何与女生聊天",
   "correct-mindset": "正确的心态",
+  "action-instead-of-speech": "喜欢不要说出来",
 };
